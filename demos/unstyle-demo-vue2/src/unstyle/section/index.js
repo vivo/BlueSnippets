aba954f47b112dcd1a8499d74c0ffdddd44c7a65
Night Sun <ismb@qq.com>
2025-03-18 20:17:55 +0800

    reset: 删除 unstyle-demo

--- demos/unstyle-demo-vue2/src/unstyle/section/index.js
+++
@@ -1,7 +0,0 @@
-import Main from './src/main.vue';
-
-Main.install = function(Vue) {
-    Vue.component(Main.name, Main);
-};
-
-export default Main;
--- demos/unstyle-demo-vue3/src/unstyle/module/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue';
-import Main from './src/main.vue';
-
-Main.install = function(app: App) {
-    app.component(Main.name, Main);
-};
-
-export default Main;
